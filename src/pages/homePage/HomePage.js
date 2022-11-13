b73a73c850a53721967d61978d718271ed14faaf
Stacey <stacey.graham1106@gmail.com>
2022-11-12 20:35:28 -0500

    added memberRoadmap component

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Mission from '../mission/Mission'
+import Roadmap from '../roadmap/Roadmap'
 
 const HomePage = () => {
   return (
     <div>
         <h1>Hey!, You are Landed on HomePage, Congrats !!</h1>
         <Mission />
+        <Roadmap />
     </div>
   )
 }
---
+++ src/pages/roadmap/Roadmap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Roadmap = () => {
+    return (
+        <div>
+            <h1>Roadmap</h1>
+            <p>This is the Member Roadmap.</p>
+        </div>
+    ); 
+};
+
+export default Roadmap;
